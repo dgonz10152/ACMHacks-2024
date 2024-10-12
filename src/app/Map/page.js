3db5da01e118a53456a12dd0d0dcf187b00f211b
dgonz10152 <daniel.gonzalez10152@gmail.com>
2024-10-12 14:20:23 -0700

    updated packages

--- src/app/Map/page.js
+++ src/app/Map/page.js
@@ -1,25 +1,13 @@
 "use client";
 
-import {
-	AppBar,
-	Box,
-	Divider,
-	Drawer,
-	IconButton,
-	List,
-	ListItemButton,
-	ListItemIcon,
-	ListItemText,
-	Typography,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import InfoIcon from "@mui/icons-material/Info";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import { useState, useCallback } from "react";
+import MenuBar from "../components/MenuBar";
 
 const containerStyle = {
 	width: "100vw",
 	height: "95vh",
+	top: "5vh",
 };
 const center = {
 	lat: 36.998714,
@@ -31,8 +19,7 @@ function Map() {
 		id: "google-map-script",
 		googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY,
 	});
-
-	console.log("Is Google Maps API loaded?", isLoaded);
+	console.log("API Key:", process.env.NEXT_PUBLIC_MAPS_API_KEY);
 
 	const [map, setMap] = useState(null);
 
@@ -51,49 +38,9 @@ function Map() {
 			zoom={10} // Added zoom level
 			onLoad={onLoad}
 			onUnmount={onUnmount}
-			className="top-[5vh]"
 		></GoogleMap>
 	) : (
 		<p>Loading map...</p> // Added fallback UI during loading
-	);
-}
-
-function MenuBar() {
-	const [open, setOpen] = useState(false);
-	const toggleDrawer = (newOpen) => () => {
-		setOpen(newOpen);
-	};
-
-	return (
-		<>
-			<Box>
-				<AppBar className="flex-row h-[5vh]">
-					<IconButton onClick={toggleDrawer(true)}>
-						<MenuIcon></MenuIcon>
-					</IconButton>
-				</AppBar>
-			</Box>
-			<Drawer className="w-full" open={open} onClose={toggleDrawer(false)}>
-				<DrawerInfo />
-			</Drawer>
-		</>
-	);
-}
-
-function DrawerInfo() {
-	return (
-		<Box className="w-56">
-			<Typography variant="h5">Basic Needs Facilities</Typography>
-			<Divider />
-			<List>
-				<ListItemButton href="/" className="p-6 m-2" variant="contained">
-					<ListItemIcon>
-						<InfoIcon />
-					</ListItemIcon>
-					<ListItemText>About</ListItemText>
-				</ListItemButton>
-			</List>
-		</Box>
 	);
 }
 
