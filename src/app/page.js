9423063654bfc9883d97b54d030d446af0a1b778
YMoraes092004 <ymoraes@ucsc.edu>
2024-10-12 14:12:39 -0700

    about files

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
-import Image from "next/image";
 import * as React from "react";
 import Button from "@mui/material/Button";
+import Link from "next/link";
+
 
 function ButtonUsage() {
 	return <Button variant="contained">Hello world</Button>;
@@ -11,9 +12,11 @@ export default function Home() {
 		<>
 			<h1 className="p-10 flex justify-center text-red-400">Home Page</h1>
 			<a href="/locations">
-				<Button variant="outlined">Check out our locations</Button>
-				<p>Testing</p>
+				<center><Button variant="outlined">Check out our locations</Button></center>
 			</a>
+			<ul>
+				<center><Button href="/About/">ABOUT US</Button></center>
+			</ul>
 		</>
 	);
 }
