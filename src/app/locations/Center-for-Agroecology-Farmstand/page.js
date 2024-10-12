fcf8cf7f75bcbb391a196233eb2d673734fa0408
ehoner23 <ehoner@ucsc.edu>
2024-10-12 13:46:45 -0700

    created base pages for cowell coffee shop and CFAE

--- src/app/locations/Center-for-Agroecology-Farmstand/page.js
+++ src/app/locations/Center-for-Agroecology-Farmstand/page.js
@@ -1,16 +1,20 @@
-"use client";
+import PageTemplate from "../templateSite/page";
 
-import { Button } from "@mui/material";
-import { usePathname } from "next/navigation";
+const facilityConfig = {
+	Name: "Center for Agroecology Farmstand",
+	Location: "PLACE",
+	About: "Rooted in the Division of Social Sciences at UC Santa Cruz, the Center for Agroecology is an organization of staff, faculty, and students who work to advance agroecology and equitable food systems through experiential education, participatory research, agricultural extension, and public service.",
+	Monday_hours: "Closed",
+	Tuesday_hours: "Closed",
+	Wednesday_hours: "Closed",
+	Thursday_hours: "12:30PM - 6PM",
+	Friday_hours: "Closed",
+	Saturday_hours: "Closed",
+	Sunday_hours: "Closed",
+	
+}
 
-export default function Home() {
-	const pathName = usePathname();
-	return (
-		<div>
-			<h1 className="p-10 flex justify-center">{pathName}</h1>
-			<a href="/">
-				<Button variant="outlined">Home</Button>
-			</a>
-		</div>
-	);
+export default function Home(){
+	return(<PageTemplate config={facilityConfig} />)
+
 }
--- src/app/locations/Cowell-Coffee-Shop/page.js
+++ src/app/locations/Cowell-Coffee-Shop/page.js
@@ -1,16 +1,21 @@
-"use client";
+import PageTemplate from "../templateSite/page";
 
-import { Button } from "@mui/material";
-import { usePathname } from "next/navigation";
+const facilityConfig = {
+	Name: "Cowell Coffee Shop",
+	Location: "PLACE",
+	About: "The Cowell Coffee Shop: For the Peoples is part of the Center's student leadership development model and serves as a cafe designed to increase student food access and support with basic needs resources. The Center's basic needs collaborations seek to put together online curriculum, workshops, conversations around food insecurity, and cooking classes designed specifically around the UCSC food system. The Center for Agroecology and Colleges, Housing and Educational Services have partnered to transform the Cowell Coffee Shop into a non-transactional cafe.",
+	Monday_hours: "10AM - 3:30PM",
+	Tuesday_hours: "10AM - 3:30PM",
+	Wednesday_hours: "10AM - 3:30PM",
+	Thursday_hours: "10AM - 3:30PM",
+	Friday_hours: "10AM - 3:30PM",
+	Saturday_hours: "Closed",
+	Sunday_hours: "Closed",
+	Products_offered: "rice\nmilk\nvegetables\nchicken\nsoap"
+	
+}
 
-export default function Home() {
-	const pathName = usePathname();
-	return (
-		<div>
-			<h1 className="p-10 flex justify-center">{pathName}</h1>
-			<a href="/">
-				<Button variant="outlined">Home</Button>
-			</a>
-		</div>
-	);
+export default function Home(){
+	return(<PageTemplate config={facilityConfig} />)
+
 }
