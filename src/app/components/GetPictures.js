59cebe7bb8a903050fc0ddf753b2155bca3d32e4
dgonz10152 <daniel.gonzalez10152@gmail.com>
2024-10-12 21:51:39 -0700

    added firebase stuff

---
+++ src/app/components/GetPictures.js
@@ -0,0 +1,40 @@
+"use client";
+
+import { storage } from "@/app/firebaseConfig";
+import { Box } from "@mui/material";
+import { listAll, ref, getDownloadURL } from "firebase/storage";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function GetPictures(location) {
+	const [images, setImages] = useState([]);
+
+	useEffect(() => {
+		const fetchImages = async () => {
+			const imagesRef = ref(storage, `${location}/`);
+
+			try {
+				const result = await listAll(imagesRef);
+				const urls = await Promise.all(
+					result.items.map((item) => getDownloadURL(item))
+				);
+				setImages(urls);
+			} catch (error) {
+				console.log("Error fetching images: ", error);
+			}
+		};
+		fetchImages();
+	}, []);
+
+	return (
+		<Box>
+			<Box className="flex">
+				{images.map((url, index) => (
+					<Box key={url} className="p-3">
+						<Image src={url} alt="Picture" width={300} height={300} />
+					</Box>
+				))}
+			</Box>
+		</Box>
+	);
+}
